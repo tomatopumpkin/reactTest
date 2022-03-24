4f83674525d438ba88b49ee1408812d22f901f4d
tomatopumpkin <6206021621147@fitm.kmutnb.ac.th>
2022-03-24 20:04:36 +0700

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ContactPage from './page/contact/contact.component';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<ContactPage />);
+  const linkElement = screen.getByText(/ติดต่อสอบถามเพิ่มเติม/i);
   expect(linkElement).toBeInTheDocument();
 });
